a7cb4102b30742de48886ff933fc734146789afb
SiddharthNigade <88178317+SiddharthNigade@users.noreply.github.com>
2025-03-24 18:17:34 -0400

    cat.js

---
+++ src/backend/test/models/cat.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const catSchema = new mongoose_1.Schema({
+    name: String,
+    weight: Number,
+    age: Number
+});
+const Cat = (0, mongoose_1.model)('Cat', catSchema);
+exports.default = Cat;
+//# sourceMappingURL=cat.js.map
