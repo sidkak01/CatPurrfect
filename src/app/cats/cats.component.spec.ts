5b3c4b1ea512b5980d72707b4cf7ee4c1729ae32
Riddhi Banerjee <riddhibanerjee@ufl.edu>
2025-04-20 22:24:27 -0400

    Update cats component unit tests

--- src/app/cats/cats.component.spec.ts
+++ src/app/cats/cats.component.spec.ts
@@ -96,7 +96,73 @@ describe('CatsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should load cats when user goes to cats page', () => {
+    const mockCats = [
+      { name: 'Whiskers', weight: '5', age: '2', breed: 'Bengal', _id: 'abc' }
+    ];
+    const catService = TestBed.inject(CatService);
+    spyOn(catService, 'getUserCats').and.returnValue(of(mockCats));
+    localStorage.setItem('userId', '123');
+  
+    component.ngOnInit(); // It is setup so when the component first loads, the cats API is called to load the current cats from the db
+  
+    expect(catService.getUserCats).toHaveBeenCalledWith('123');
+    expect(component.cats.length).toBe(1);
+    expect(component.cats[0].name).toBe('Whiskers');
+  });
+
+  it('Should add a new cat', () => {  // Basic adding a cat
+    const catService = TestBed.inject(CatService);
+    spyOn(catService, 'addCat').and.callThrough();
+    localStorage.setItem('userId', '123');
+  
+    component.newCat = {
+      name: 'Mittens',
+      weight: '6',
+      age: '3',
+      breed: 'Siamese'
+    };
+  
+    component.addCat();
+  
+    expect(catService.addCat).toHaveBeenCalled();
+  });
+
+  it('Should update an existing cat', () => {
+    const catService = TestBed.inject(CatService);
+    spyOn(catService, 'updateCat').and.callThrough();
+  
+    const existingCat = {
+      name: 'Tom',
+      weight: '7',
+      age: '4',
+      breed: 'Persian',
+      _id: 'cat123' // Existing cats are linked in the db with the user's _id
+    };
+  
+    component.cats = [existingCat];
+    component.newCat = { ...existingCat, name: 'Tommy' };
+  
+    component.addCat();
+  
+    expect(catService.updateCat).toHaveBeenCalledWith('cat123', jasmine.any(Object)); // Make sure it is properly updated in db by using the _id
+  });
+
+  it('Should delete a cat after confirmation', () => {
+    spyOn(window, 'confirm').and.returnValue(true); // Confirmation is required to delete the cat
+    const catService = TestBed.inject(CatService);
+    spyOn(catService, 'deleteCat').and.callThrough();
+  
+    const catToDelete = { name: 'Fluffy', weight: 5, age: 3, breed: 'Persian', _id: '12345' };
+    component.cats = [catToDelete];
+  
+    component.deleteCat(catToDelete, new MouseEvent('click'));
+  
+    expect(catService.deleteCat).toHaveBeenCalledWith('12345');
+  });
+
 });
