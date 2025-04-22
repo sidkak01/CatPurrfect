7fddc9717139ad1d332efd49a012ed6e9c01a728
SiddharthNigade123 <siddharthnigade@conveng.com>
2025-04-21 21:23:00 -0400

    testing

--- src/backend/test/models/cat.js
+++ src/backend/test/models/cat.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 const catSchema = new mongoose_1.Schema({
-    name: String,
-    weight: Number,
-    age: Number
+  name: String,
+  weight: Number,
+  age: Number,
 });
-const Cat = (0, mongoose_1.model)('Cat', catSchema);
+const Cat = (0, mongoose_1.model)("Cat", catSchema);
 exports.default = Cat;
+// commenting
 //# sourceMappingURL=cat.js.map
